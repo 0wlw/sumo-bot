f23d11693dec595f43e43ebb8a01aff45accabd4
0wlw <142946217+0wlw@users.noreply.github.com>
2026-02-24 10:59:10 -0800

    create main.ino

---
+++ main.ino
@@ -0,0 +1,86 @@
+#include "motor.h"
+
+const int motorRENPin = 10;
+const int motorRIN2Pin = 9;
+const int motorRIN1Pin = 8;
+
+const int motorLENPin = 5;
+const int motorLIN2Pin = 7;
+const int motorLIN1Pin = 6;
+
+const int driveSpeed = 100;
+const int turnSpeed = 100;
+
+Motor rightMotor(motorRIN1Pin, motorRIN2Pin, motorRENPin);
+Motor leftMotor(motorLIN1Pin, motorLIN2Pin, motorLENPin);
+
+int LineSensorFront, LineSensorBack;
+
+enum State {
+  IDLE,
+  SEARCH,
+  PUSH,
+  AVOIDEDGE,
+  CHARGE,
+  END
+};
+
+State currentState = IDLE;
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(5000);
+}
+
+void loop() {
+  avoid_edge();
+  if (digitalRead(2) < 1) {
+    currentState = CHARGE;
+    attack();
+  }
+
+
+  
+}
+
+void avoid_edge() {
+  LineSensorFront = digitalRead(4);
+  LineSensorBack = digitalRead(3);
+  
+  if (LineSensorFront < 1) {
+    rightMotor.backward(driveSpeed);
+    leftMotor.backward(driveSpeed);
+    delay(1000);
+    rightMotor.forward(driveSpeed);
+    leftMotor.backward(driveSpeed);
+    delay(100);
+  } else if (LineSensorFront < 1) {
+    rightMotor.forward(driveSpeed);
+    leftMotor.forward(driveSpeed);
+    delay(1000);
+    rightMotor.forward(driveSpeed);
+    leftMotor.backward(driveSpeed);
+    delay(100);
+  } else {
+    DriveAround();
+  }
+}
+
+void DriveAround() {
+  rightMotor.forward(driveSpeed);
+  leftMotor.forward(driveSpeed);
+}
+
+void CHECK_IF_END() {
+  if (millis() > 61000) {
+    currentState = END;
+  }
+}
+
+void attack() {
+  attackDriveSpeed = 255;
+  rightMotor.forward(attackDriveSpeed);
+  leftMotor.forward(attackDriveSpeed);
+  delay(1000);
+}
