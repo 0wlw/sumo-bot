037c24c1624378228c1e9a066c667ce091baefd1
0wlw <142946217+0wlw@users.noreply.github.com>
2026-03-02 09:31:08 -0800

    Update main4.ino

--- main4.ino
+++ main4.ino
@@ -1,29 +1,43 @@
 #include "motor.h"
 
-const int motorRENPin = 10;
-const int motorRIN2Pin = 9;
-const int motorRIN1Pin = 8;
+//Motors
+const int motorRENPin = 41;
+const int motorRIN2Pin = 39;
+const int motorRIN1Pin = 37;
 
-const int motorLENPin = 5;
-const int motorLIN2Pin = 7;
-const int motorLIN1Pin = 6;
-
-const int driveSpeed = 235;
-const int turnSpeed = 235; 				/// 235 turnspeed for 1500ms gives ~150 degrees rotation
-
-const int trigPin = 9;
-const int echoPin = 10;
-
-float duration, distance;
-float array[10];
-float array_min_val = 999999.0;
-float direction, calbirated_val;
+const int motorLENPin = 43;
+const int motorLIN2Pin = 45;
+const int motorLIN1Pin = 47;
 
 Motor rightMotor(motorRIN1Pin, motorRIN2Pin, motorRENPin);
 Motor leftMotor(motorLIN1Pin, motorLIN2Pin, motorLENPin);
 
+const int motorRENPin2 = 31;
+const int motorRIN2Pin2 = 35;
+const int motorRIN1Pin2 = 33;
+
+const int motorLENPin2 = 53;
+const int motorLIN2Pin2 = 51;
+const int motorLIN1Pin2 = 49;
+
+Motor rightMotor2(motorRIN1Pin2, motorRIN2Pin2, motorRENPin2);
+Motor leftMotor2(motorLIN1Pin2, motorLIN2Pin2, motorLENPin2);
+
+const int driveSpeed = 150;
+const int turnSpeed = 150; 				/// 235 turnspeed for 1500ms gives ~150 degrees rotation
+
+// for search and orient
+const int trigPin = 9;
+const int echoPin = 10;
+float duration, distance;
+float array[100];
+float array_min_val = 999999.0;
+float direction, calbirated_val;
+float locationData[100];
+
+//edge detector pin
 int frontDetectorPin = 2;
-//int backDetectorPin = 3;
+
 
 enum State {
   FORWARD,
@@ -33,7 +47,7 @@ enum State {
   SEARCH,
 };
 
-State currentState = FORWARD;
+State currentState = SEARCH;
 
 void setup() {
   Serial.begin(9600);
@@ -41,8 +55,8 @@ void setup() {
 
 void loop() {
   switch (currentState) {
-    case FORWARD:
-      moveForward()
+    case FORWARD:             //move forward until reaching edge of arena
+      moveForward();
 
       if (digitalRead(frontDetectorPin) == LOW) {
         currentState = AVOID;
@@ -50,27 +64,31 @@ void loop() {
       break;
     
 
-    case BACKWARD:
+    case BACKWARD:              //currently only used when avoiding edge
       moveBackward();
       delay(2000);
       currentState = TURN;
+      break;
 		
 
-		case TURN:
+		case TURN:              //currently only used when avoiding edge after having moved backward already
 			turnLeft();
+      delay(1500);
+      currentState = FORWARD;
+      break;
     
 
     case AVOID:
-      moveBackward()
-      delay(3000);
-      turnLeft()
-      delay(1500);
-      currentState = FORWARD;
+      currentState = BACKWARD;
       break;
 
 
     case SEARCH:
-			ultrasonicSearch()
+			locationData[100] = ultrasonicSearch();
+      orient(locationData);
+      currentState = FORWARD;
+      break;
+
   }
 }
 
@@ -78,16 +96,22 @@ void loop() {
 void turnLeft() {
     rightMotor.forward(turnSpeed);
     leftMotor.backward(turnSpeed);
+    rightMotor2.forward(turnSpeed);
+    leftMotor2.backward(turnSpeed);
 }
 
 void moveForward() {
     rightMotor.forward(driveSpeed);
     leftMotor.forward(driveSpeed);
+    rightMotor2.forward(driveSpeed);
+    leftMotor2.forward(driveSpeed);
 }
 
 void moveBackward() {
-    rightMotor.backward(driveSpeed);
-    leftMotor.backward(driveSpeed);
+    rightMotor.backward(255);
+    leftMotor.backward(255);
+    rightMotor2.backward(255);
+    leftMotor2.backward(255);
 }
 
 
@@ -111,15 +135,19 @@ float ultrasonicSearch() {
       leftMotor.backward(driveSpeed);
       delay(36);
     }
-		return array;
+		return array[100];
 }
 
-float determineOrientation() {
-	for (int i; i < 10; i++) {
+void orient(float array[10]) {
+  int i;
+	for (i; i < 100; i++) {
 		if (array[i] < array_min_val) {
 			array_min_val = array[i];
 			direction = i;
 		}
 	}
-	rightMotor.forward()
+  Serial.println(i * 10);
+	rightMotor.forward(turnSpeed);
+  leftMotor.backward(turnSpeed);
+  delay(i * 10);
 }
